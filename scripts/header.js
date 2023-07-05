515f1d7b084a0c0d44fc97aa8af39679d2e1ed66
Ihor <frunza2029@gmail.com>
2023-07-05 19:43:11 +0300

    add adaptive and burger-menu

--- scripts/header.js
+++ scripts/header.js
@@ -1,6 +1,25 @@
-window.addEventListener('scroll', function() {
-    let header = this.document.querySelector('header')
+const header = document.querySelector('.header')
+const menu = document.querySelector('.burger__menu')
+const menuIcon = document.querySelector('.menuIcon')
+const closeIcon = document.querySelector('.closeIcon')
+const hamburger = document.querySelector('.humburger')
 
+function toggleMenu () {
+    if (menu.classList.contains('showMenu')) {
+        menu.classList.remove('showMenu')
+        closeIcon.style.display = 'none'
+        menuIcon.style.display = 'block'
+        header.style.background = 'none'
+    } else {
+        menu.classList.add('showMenu')
+        closeIcon.style.display = 'block'
+        menuIcon.style.display = 'none'
+        header.style.background = '#181E1A'
+    }
+}
+hamburger.addEventListener('click', toggleMenu)
+
+window.addEventListener('scroll', function() {
     if (this.window.scrollY > 0) {
         header.style.background = '#181E1A'
     } else {
